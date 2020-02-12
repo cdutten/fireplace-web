1f4a08b14f26d2fc2dcad37cc18806314a2fff56
cdutten <cdutten@medallia.com>
2020-02-11 22:09:57 -0300

    Testing linting circleci

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Search from "./screens/Search";
-
-
+import Search from './screens/Search';
 
 const App = () => {
-
-    return (<div>
-        <Search/>
-    </div>);
+  return (<div>
+    <Search/>
+  </div>);
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'))
-
-
+ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Fireplace",
-            template: './src/index.html'
-        }),
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Fireplace',
+      template: './src/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
